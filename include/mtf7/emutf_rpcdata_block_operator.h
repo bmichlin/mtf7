f2920f42d8345c65645f5cb17954bc1d9dd12b2e
Pierluigi Bortignon <bortigno@gmail.com>
2014-12-04 18:26:00 +0100

    included unpacking for rpc data

---
+++ include/mtf7/emutf_rpcdata_block_operator.h
@@ -0,0 +1,25 @@
+#ifndef __EMUTF_RPCDATA_BLOCK_OPERATOR__
+#define __EMUTF_RPCDATA_BLOCK_OPERATOR__
+
+#include "mtf7/types.h"
+#include "mtf7/emutf_block_operator.h"
+
+namespace mtf7{
+
+  class emutf_rpcdata_block_operator: public emutf_block_operator{
+
+  public:
+    
+    emutf_rpcdata_block_operator ( error_value *error_status, emutf_event *unpacked_event_info ):
+        emutf_block_operator( error_status, unpacked_event_info ) {}
+    virtual unsigned long        pack   ( );
+    virtual const word_64bit    *unpack (const word_64bit *at_ptr);
+
+    static const unsigned int _nominal_buffer_size = 1; // buffer size in 64-bit words
+
+  };
+
+}
+
+
+#endif
